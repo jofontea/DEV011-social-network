a8da5cfe8063affbd60dc8abcd04adbcac441cd9
Josefa Fontecilla <fontecillajosefa10@gmail.com>
2023-10-19 14:51:35 -0300

    creating the js archives (home, error, login) and trying to connect them

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 // Este es el punto de entrada de tu aplicacion
+// file main.js
+import home from "./home.js";
+import login from "./login.js";
+import error from "./error.js";
+//import { myFunction } from './lib/index.js';
 
-import { myFunction } from './lib/index.js';
+//myFunction();
+const routes = [
+  { path: "/", component: home },
+  { path: "/login", component: login },
+  { path: "/error", component: error },
+];
+const defaultRoute = "/";
+const root = document.getElementById("root");
 
-myFunction();
+function navigateTo(hash) {
+  const route = routes.find((routeFound) => routeFound.path === hash);
+
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path
+    );
+    if (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+    root.appendChild(route.component());
+  }
+}
+function navigateTo(route) {
+    
+  console.log(`Navigating to ${route}`);
+}
